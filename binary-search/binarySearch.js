22263942d5489e730ed787af34e52d21b28ce676
RHRABIN <rhrabin934602@gmail.com>
2023-03-02 10:46:07 +0600

    binray search new function implement for ascending descending data

--- binary-search/binarySearch.js
+++ binary-search/binarySearch.js
@@ -26,4 +26,36 @@ const binarySearch = (arr, target) => {
     return "Targe is not found!";
 };
 
-console.log(binarySearch(arr, 909));
+// console.log(binarySearch(arr, 909));
+const array2 = [50, 45, 40, 35, 30, 5, 10, 15, 20];
+
+const descendingSearch = (array, target) => {
+    let start = 0;
+    let end = array.length - 1;
+    let mid = Math.floor((end + start) / 2);
+    let isAscending = array[0] < array[array.length - 1];
+
+    while (start <= end) {
+        mid = Math.floor((end + start) / 2);
+        isAscending = array[start] < array[end];
+
+        if (array[mid] === target) {
+            return "Target is found index: " + mid;
+        } else if (isAscending) {
+            if (array[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        } else {
+            if (array[mid] < target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+    }
+
+    return "Targe is not found!";
+};
+console.log(descendingSearch(array2, 15));
